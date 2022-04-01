b8f8a38a89e8d6d56c8c6ac72f3c786b32fdee4a
dehre <loris4526@gmail.com>
2022-04-01 16:26:47 +0200

    documentation for global functions

--- include/sht21.h
+++ include/sht21.h
@@ -9,9 +9,33 @@ typedef enum
     sht21_i2c_speed_fast = 400000,
 } sht21_i2c_speed_t;
 
-esp_err_t sht21_init(i2c_port_t i2c_port, gpio_num_t sda_pin,
-                     gpio_num_t scl_pin, sht21_i2c_speed_t i2c_speed);
+/**
+ * @brief Initialize the I2C peripheral.
+ *
+ * @param i2c_num I2C port to configure
+ * @param sda_pin GPIO number for I2C sda signal
+ * @param scl_pin GPIO number for I2C scl signal
+ * @param i2c_speed I2C clock frequency for master mode
+ *
+ * @return esp_err_t
+ */
+esp_err_t sht21_init(i2c_port_t i2c_num, gpio_num_t sda_pin, gpio_num_t scl_pin,
+                     sht21_i2c_speed_t i2c_speed);
 
-esp_err_t sht21_get_temperature(float *ans);
+/**
+ * @brief Read the sensor and calculate the relative humidity [%RH].
+ *
+ * @param dst pointer to destination variable
+ *
+ * @return esp_err_t
+ */
+esp_err_t sht21_get_temperature(float *dst);
 
-esp_err_t sht21_get_humidity(float *ans);
+/**
+ * @brief Read the sensor and calculate the temperature [°C].
+ *
+ * @param dst pointer to destination variable
+ *
+ * @return esp_err_t
+ */
+esp_err_t sht21_get_humidity(float *dst);
